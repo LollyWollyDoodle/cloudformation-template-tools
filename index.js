d358c37c92a4681bb885030da78aa2c15ca37bd9
Geoff Mishkin <geoff@lollywollydoodle.com>
2017-01-23 15:37:50 -0500

    Change to gulp plugin
    The plugin outputs validation data to be used in gulp.dest() stream
    Then callers can use gulp-newer to skip validation API call if file is newer than template JSON

--- index.js
+++ index.js
@@ -2,8 +2,35 @@ const AWS = require("aws-sdk");
 AWS.config.update({ region: require("process").env.AWS_REGION });
 const cf = new AWS.CloudFormation({ apiVersion: "2010-05-15" });
 
+const gutil = require("gulp-util");
+const PluginError = gutil.PluginError;
+const through2 = require("through2");
+const Vinyl = require("vinyl");
+
+const PLUGIN_NAME = "cloudformation-template-tools";
+
 module.exports = {
-	validate: function (body, cb) {
-		cf.validateTemplate({ TemplateBody: body }, cb);
+	validate: function () {
+		return through2.obj(function (file, encoding, cb) {
+			if (file.isStream()) {
+				cb(new PluginError(PLUGIN_NAME, "Streams aren't supported"));
+			}
+			else {
+				cf.validateTemplate({ TemplateBody: file.contents.toString(encoding) }, function (err, data) {
+					if (err === null) {
+						var validated = new Vinyl({
+							base: file.base,
+							path: file.base + "/" + file.relative + ".validated"
+						});
+
+						validated.contents = Buffer.from(JSON.stringify(data), "UTF-8");
+						cb(null, validated);
+					}
+					else {
+						cb(err);
+					}
+				});
+			}
+		});
 	}
 };
